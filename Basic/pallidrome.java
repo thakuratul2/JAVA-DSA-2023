d08d8fc319ea824627919066b4d4b0f7987ffe58
thakuratul2 <pratapsinghatul111@gmail.com>
2023-02-08 20:00:40 +0530

    Basic

---
+++ Basic/pallidrome.java
@@ -0,0 +1,27 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class pallidrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+
+        int rev = 0;
+        int temp=num;
+
+        int rem;
+        while(num>0){
+            rem = num%10;
+            rev = (rev*10) +rem;
+            num = num/10;
+        }
+        if(temp==rev){
+            System.out.println("Pallidrome");
+        }else{
+            System.out.println("No");
+        }
+        sc.close();
+    }
+}
---
+++ Basic/reverse_number.java
@@ -0,0 +1,20 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class reverse_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        int rev = 0;
+        while (num>0) {
+            int temp = num%10;
+            rev = (rev*10)+temp;
+            num = num/10;
+        }
+
+        System.out.println("Reverse: "+rev);
+        sc.close();
+    }
+}
