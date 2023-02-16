746f5b88d023ebbfba54d3abd3a089edd8077e82
thakuratul2 <pratapsinghatul111@gmail.com>
2023-02-16 23:11:20 +0530

    String

---
+++ Strings/Roman_to_Integer.java
@@ -0,0 +1,13 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Roman_to_Integer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String name = sc.nextLine();
+
+        sc.close();
+    }
+}
