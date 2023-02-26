471f9af91368b838fb5cc0105a339f3a90330f96
thakuratul2 <pratapsinghatul111@gmail.com>
2023-02-26 11:07:50 +0530

    Array

---
+++ Arrays/rotate_array.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class rotate_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = sc.nextInt();
+
+        int nums[] = new int[size];
+        System.out.println("Enter the element: ");
+        for(int i = 0;i<size;i++){
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the times to rotate: ");
+        int k = sc.nextInt();
+
+        int rotation = k%size;
+
+        rotate(nums,0,size-1);
+        rotate(nums,0,rotation-1);
+        rotate(nums,rotation,size-1);
+    }
+    
+   private void rotate(int[] arr,int low,int high){
+        int temp = arr[low];
+        nums[low] = nums[high];
+        nums[high] = temp;
+
+        low++;
+        high--;
+        
+    }
+    sc.close();
+}
