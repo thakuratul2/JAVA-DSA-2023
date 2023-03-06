af77094b34d18f0b3a6bfac6dee890606a6e9e11
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-06 22:04:13 +0530

    Linklist

--- Linklist/merge_sorted.java
+++ Linklist/merge_sorted.java
@@ -1,40 +1,43 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
-    class ListNode{
+    class ListNode {
         int val;
-        ListNode next; 
+        ListNode next;
 
-        ListNode(int val){
+        ListNode(int val) {
             this.val = val;
             this.next = null;
         }
     }
+
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(0);
         ListNode head = dummy;
 
-        while(list1!=null && list2!=null){
-            if(list1.val<list2.val){
- dummy.next = list1;
-            list1 = list1.next;
-            }else{
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                dummy.next = list1;
+                list1 = list1.next;
+            } else {
                 dummy.next = list2;
                 list2 = list2.next;
             }
             dummy = dummy.next;
-           
+
         }
-        if(list1!=null) dummy.next = list1;
-        if(list2!=null) dummy.next  =list2;
+        if (list1 != null)
+            dummy.next = list1;
+        if (list2 != null)
+            dummy.next = list2;
 
         return head.next;
     }
