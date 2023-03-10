a3f69d95aaa73fd870ff49f64f1e48ad8aa973b6
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-10 14:00:25 +0530

    Linklist

--- Linklist/delete_node.java
+++ Linklist/delete_node.java
@@ -1,5 +1,23 @@
-public class delete_node {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    class ListNode{
+        int val;
+        ListNode next;
+
+        ListNode(int val){
+            this.val = val;
+            this.next = null;
+        }
+    }
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
