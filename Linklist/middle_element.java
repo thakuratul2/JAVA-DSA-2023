8138658448f38d068c8d313d786d8ef49c42d366
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-10 14:19:47 +0530

    Linklist

--- Linklist/middle_element.java
+++ Linklist/middle_element.java
@@ -1,5 +1,37 @@
-public class middle_element {
-    public static void main(String[] args) {
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution2 {
+   class ListNode{
+        int val;
+        ListNode next;
+
+        ListNode(int val){
+            this.val = val;
+            this.next = null;
+        }
+
+    }
+
+    public ListNode middleNode(ListNode head) {
+
+        //use tortise approach
+        ListNode slow = head;
+        ListNode fast = head;
+
         
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
