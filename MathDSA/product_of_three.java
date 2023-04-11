bf0433d582edc7ca1c5618184674f8a751b9a364
thakuratul2 <pratapsinghatul111@gmail.com>
2023-04-11 22:14:12 +0530

    Math

--- MathDSA/product_of_three.java
+++ MathDSA/product_of_three.java
@@ -1,7 +1,26 @@
 package MathDSA;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class product_of_three {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = sc.nextInt();
+
+        int nums[] = new int[size];
+
+        System.out.println("Enter the elements: ");
+        for(int i=0;i<size;i++){
+            nums[i] = sc.nextInt();
+        }
+        Arrays.sort(nums);
+
         
+        int ans = Math.max(nums[size-1]*nums[size-2]*nums[size-3], nums[size-1]*nums[0]*nums[1]);
+
+        System.out.println("Maxium product: "+ans);
+        sc.close();
     }
 }
