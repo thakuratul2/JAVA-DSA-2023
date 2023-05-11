1d8883587393b83eb652827df76ecec9e0eeb75c
thakuratul2 <pratapsinghatul111@gmail.com>
2023-05-11 19:57:38 +0530

    DSA

--- Linklist/reverse_node.java
+++ Linklist/reverse_node.java
@@ -1,5 +1,22 @@
-public class reverse_node {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+public class reverse_node<ListNode> {
+
+    class ListNode {
+        int data;
+        ListNode next;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+
+            // update
+            prev = current;
+            current = next;
+        }
+        return prev;
     }
 }
