816d63cdf9a87eac0d5c2b7ca59a3472b1fb5119
thakuratul2 <pratapsinghatul111@gmail.com>
2023-05-12 21:41:10 +0530

    Searching

--- Searching/intersection_two_array.java
+++ Searching/intersection_two_array.java
@@ -1,5 +1,24 @@
+import java.util.HashSet;
+
 public class intersection_two_array {
-    public static void main(String[] args) {
-        
+    
+        public int[] intersection(int[] nums1, int[] nums2) {
+            
+              HashSet<Integer> set=new HashSet<>();
+            for(int i=0;i<nums1.length;i++){
+                for(int j=0;j<nums2.length;j++){
+                    if(nums1[i]==nums2[j]){
+                        set.add(nums1[i]);  //add unique elements only
+                    }
+                }
+            }
+            int arr[] =new int[set.size()];
+            int j =0;         //for itteration in array
+           
+             for(int i :set){
+             arr[j] =i;
+             j++;
+         }
+            return arr;
+        }
     }
-}
