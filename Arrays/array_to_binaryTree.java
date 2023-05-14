ca03d0676d53e8f348681a1d745fb597e39f51e2
thakuratul2 <pratapsinghatul111@gmail.com>
2023-05-14 18:50:28 +0530

    DSA

---
+++ Arrays/array_to_binaryTree.java
@@ -0,0 +1,28 @@
+public class array_to_binaryTree<TreeNode> {
+  class TreeNode{
+    int data;
+    TreeNode next;
+  }
+    public TreeNode sortedArrayToBST(int[] nums) {
+          if (nums.length == 0) {
+        return null;
+    }
+
+    TreeNode head = helper(nums, 0, nums.length - 1);
+    return head;
+}
+
+public TreeNode helper(int[] nums, int low, int high) {
+    if (low > high) { 
+        return null;
+
+    }
+
+    int mid = (low + high) / 2;
+    TreeNode node = new TreeNode();
+    node.next = helper(nums, low, mid - 1);
+    node.next = helper(nums, mid + 1, high);
+    return node;
+    
+}
+    }
