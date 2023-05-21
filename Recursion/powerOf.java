5997014ef007ecf49ba47e0c680ec71244a95183
thakuratul2 <pratapsinghatul111@gmail.com>
2023-05-21 14:52:41 +0530

    DSA

--- Recursion/powerOf.java
+++ Recursion/powerOf.java
@@ -17,6 +17,7 @@ public class powerOf {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Enter the number: ");
         int x = sc.nextInt();
 
