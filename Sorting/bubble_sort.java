7b569d25220075015a48311975389e87a2fa5d77
thakuratul2 <pratapsinghatul111@gmail.com>
2023-07-03 20:11:51 +0530

    DSA

--- Sorting/bubble_sort.java
+++ Sorting/bubble_sort.java
@@ -1,7 +1,46 @@
 package Sorting;
 
+import java.util.Scanner;
+
 public class bubble_sort {
+
+    static public void bubble_code(int[] arr, int n){
+
+        for(int i =0;i<n-1;i++){
+            for(int j = 0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+            //after sorting
+            
+        }
+        System.out.println("After Sorting: ");
+            for(int i =0;i<n;i++){
+                System.out.println(arr[i]+" ");
+            }
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = sc.nextInt();
+
+        int sort[] = new int[size];
+
+        System.out.println("Enter the element: ");
+        for(int i =0;i<size;i++){
+            sort[i] = sc.nextInt();
+        }
+
+        System.out.println("Before Sorting: ");
+        for(int j = 0;j<size;j++){
+            System.out.println(sort[j]+" ");
+        }
+        System.out.println();
+        bubble_code(sort, size);
+
+        sc.close();
     }
 }
---
+++ Sorting/insertion_sort.java
@@ -0,0 +1,7 @@
+package Sorting;
+
+public class insertion_sort {
+    public static void main(String[] args) {
+        
+    }
+}
