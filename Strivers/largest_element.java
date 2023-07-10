6f6f3b942735015dd4055e9d6d9a1d1b5fec36c4
thakuratul2 <pratapsinghatul111@gmail.com>
2023-07-10 20:05:49 +0530

    DSA

---
+++ Strivers/largest_element.java
@@ -0,0 +1,31 @@
+package Strivers;
+
+import java.util.Scanner;
+
+public class largest_element {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size: ");
+        int size = sc.nextInt();
+
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter the element: ");
+        for(int i = 0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        //by using max variable
+        int max = arr[0];
+
+        for(int j=0;j<size;j++){
+            if(arr[j]>max){
+                max = arr[j];
+            }
+        }
+        System.out.println("Largest Element: ");
+        System.out.println(max+"");
+
+        sc.close();
+    }
+}
